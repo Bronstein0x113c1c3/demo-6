9a6464236e28df1094fff0f38927971c72184460
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-07-03 20:48:38 +0700

    bài của tui hơi tệ xíu, mong thông cảm :((

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,12 +1,61 @@
 package com.example.demo;
 
-public class Student extends Human {
-        private int id;
-        private String grade;
-        private String className;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-        public Student(String fullName, String CCCD, int age, int id, String grade, String className) {
-            super(fullName, CCCD, age);
+@Entity
+@Table(name = "human")
+public class Student{
+    public Student(){}
+    @Column(name = "fullname")
+    private String fullName;
+    @Column(name="cccd")
+    private String cccd;
+    @Column(name="age")
+    private int age;
+    @Id
+    @Column(name="id")
+    private int id;
+    @Column(name="grade")
+    private String grade;
+    @Column(name="classname")
+    private String className;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCccd() {
+        return cccd;
+    }
+
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public Student(String fullName, String CCCD, int age, int id, String grade, String className) {
+            this.fullName = fullName;
+            this.cccd = cccd;
+            this.age = age;
+
             this.id = id;
             this.grade = grade;
             this.className = className;
--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +11,8 @@ import java.util.List;
 @RestController
 public class StudentController {
     private List<Student> students = new ArrayList<>();
+    @Autowired
+    public StudentRepo TheRepo;
 
     public StudentController() {
         students.add(new Student("Pham A", "5", 16, 5, "10", "10B"));
@@ -21,40 +21,71 @@ public class StudentController {
         students.add(new Student("Ngoc D", "8", 16, 8, "11", "11B"));
     }
 
-    @PostMapping(value = "/students")
-    public MyAn getStudentsByClass(@RequestBody RequestStudent requestStudent) {
-        String className = requestStudent.getClassName();
-        MyAn ma = new MyAn();
-        List<Student> studentsByClass = new ArrayList<>();
+//    @PostMapping(value = "/students")
+//    public MyAn getStudentsByClass(@RequestBody RequestStudent requestStudent) {
+//        String className = requestStudent.getClassName();
+//        MyAn ma = new MyAn();
+//        List<Student> studentsByClass = new ArrayList<>();
+//
+//        if (students == null || students.size() == 0) {
+//            ma.setCode("01");
+//            ma.setMessage("Ko tim thay");
+//            ma.setLstStudent(studentsByClass);
+//            return ma;
+//        }
+//
+//        for (Student student : students) {
+//            try {
+//                if (student.getClassName().equalsIgnoreCase(className)) {
+//                    studentsByClass.add(student);
+//                }
+//            } catch (Exception ex) {
+//                ma.setCode("99");
+//                ma.setMessage("gap loi");
+//                return ma;
+//            }
+//        }
+//
+//        if (studentsByClass.isEmpty()) {
+//            ma.setCode("01");
+//            ma.setMessage("Ko tim thay");
+//        } else {
+//            ma.setCode("00");
+//            ma.setMessage("Thanh cong");
+//        }
+//
+//        ma.setLstStudent(studentsByClass);
+//        return ma;
+//    }
 
-        if (students == null || students.size() == 0) {
-            ma.setCode("01");
-            ma.setMessage("Ko tim thay");
-            ma.setLstStudent(studentsByClass);
-            return ma;
-        }
-
-        for (Student student : students) {
-            try {
-                if (student.getClassName().equalsIgnoreCase(className)) {
-                    studentsByClass.add(student);
-                }
-            } catch (Exception ex) {
-                ma.setCode("99");
-                ma.setMessage("gap loi");
-                return ma;
+    @GetMapping(value = "/students_test")
+    public ResponseEntity<List<Student>> getStudent(){
+        List<Student> res = (List<Student>) TheRepo.findAll();
+        return new ResponseEntity<>(res,HttpStatus.OK);
+    }
+    @PostMapping(value="/students_test")
+    public ResponseEntity<List<Student>> addStudent(@RequestBody Student param){
+        TheRepo.save(param);
+        List<Student> res = (List<Student>) TheRepo.findAll();
+        return new ResponseEntity<>(res,HttpStatus.OK);
+    }
+    @PutMapping(value="/students_test")
+    public ResponseEntity<List<Student>> editStudent(@RequestBody Student param){
+        List<Student> list = (List<Student>)TheRepo.findAll();
+        for (Student s : list){
+            if (param.getId()==s.getId()){
+                TheRepo.delete(s);
+                TheRepo.save(param);
+                return new ResponseEntity<>((List<Student>)TheRepo.findAll(),HttpStatus.OK);
             }
         }
-
-        if (studentsByClass.isEmpty()) {
-            ma.setCode("01");
-            ma.setMessage("Ko tim thay");
-        } else {
-            ma.setCode("00");
-            ma.setMessage("Thanh cong");
-        }
-
-        ma.setLstStudent(studentsByClass);
-        return ma;
+        return new ResponseEntity<>(null);
     }
+    @DeleteMapping(value="/students_test")
+    public ResponseEntity<String> delete(@RequestBody Student param){
+        TheRepo.deleteAll();
+        return new ResponseEntity<>("Deleted all entries",HttpStatus.OK);
+    }
+
+
 }
